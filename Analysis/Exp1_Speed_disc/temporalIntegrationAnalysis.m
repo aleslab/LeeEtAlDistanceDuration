a6193b5a5243e6dc6d8cf24ad151eb497e4bc28b
abigailrilee <abigailrilee@googlemail.com>
2019-08-22 15:36:16 +0100

    More edits to analysis code for distance/duration paper

--- Analysis/Exp1_Speed_disc/temporalIntegrationAnalysis.m
+++ Analysis/Exp1_Speed_disc/temporalIntegrationAnalysis.m
@@ -1,3 +1,7 @@
+% For psychometric analysis of the speed discrimination experiment (Exp1)
+% in "Visual perception of motion: Exploring how distance and duration
+% information contributes to speed and speed change discrimination". Run
+% this, and it then runs psychometricFitTemporal.
 
 %load data
 ptbCorgiData = uiGetPtbCorgiData();
@@ -24,32 +28,32 @@ analysis.funcOptions.xLabel     = 'speed (deg/s)';
 
 [analysis, ptbCorgiData] = ptbCorgiAnalyzeEachParticipant(analysis,ptbCorgiData);
 
-% %Now let's report the resulting standard deviations.
-% disp('Ugly, Quick, output of standard deviations:')
-% nPpt = length( ptbCorgiData.participantList);
-% for iPpt = 1:nPpt
-%
-%     disp(ptbCorgiData.participantList{iPpt})
-% %     for iContrast = 1:length(analysis.results(iPpt).conditionLabels)
-% %     disp([analysis.results(iPpt).conditionLabels{iContrast} ' std dev: ' num2str(1./analysis.results(iPpt).paramsValues(iContrast,2))])
-% %     end
-% end
-% 
-% cellToSave = [analysis.results.conditionLabels; analysis.results.fiftyPercentPoint;...
-%     analysis.results.threshold75; analysis.results.Slope; analysis.results.paramsValues; ...
-%     analysis.results.bootThresLo; analysis.results.bootThresHi; analysis.results.bootSlopeLo; ...
-%     analysis.results.bootSlopeHi];
-% 
-% tableToSave = cell2table(cellToSave);
-% participantID = char(ptbCorgiData.participantList(1));
-% 
-% dataFileName = strcat(participantID, '_', ptbCorgiData.paradigmName, '_data_new.csv');
-% 
-% writetable(tableToSave, dataFileName);
-% 
-% pdfFileName = strcat(participantID, '_', ptbCorgiData.paradigmName, '_new.pdf');
-% saveas(gcf, pdfFileName);
-% 
-% svgFileName = strcat(participantID, '_', ptbCorgiData.paradigmName, '_new.svg');
-% saveas(gcf, svgFileName);
+%Now let's report the resulting standard deviations.
+disp('Ugly, Quick, output of standard deviations:')
+nPpt = length( ptbCorgiData.participantList);
+for iPpt = 1:nPpt
+
+    disp(ptbCorgiData.participantList{iPpt})
+%     for iContrast = 1:length(analysis.results(iPpt).conditionLabels)
+%     disp([analysis.results(iPpt).conditionLabels{iContrast} ' std dev: ' num2str(1./analysis.results(iPpt).paramsValues(iContrast,2))])
+%     end
+end
+
+cellToSave = [analysis.results.conditionLabels; analysis.results.fiftyPercentPoint;...
+    analysis.results.threshold75; analysis.results.Slope; analysis.results.paramsValues; ...
+    analysis.results.bootThresLo; analysis.results.bootThresHi; analysis.results.bootSlopeLo; ...
+    analysis.results.bootSlopeHi];
+
+tableToSave = cell2table(cellToSave);
+participantID = char(ptbCorgiData.participantList(1));
+
+dataFileName = strcat(participantID, '_', ptbCorgiData.paradigmName, '_data_new.csv');
+
+writetable(tableToSave, dataFileName);
+
+pdfFileName = strcat(participantID, '_', ptbCorgiData.paradigmName, '_new.pdf');
+saveas(gcf, pdfFileName);
+
+svgFileName = strcat(participantID, '_', ptbCorgiData.paradigmName, '_new.svg');
+saveas(gcf, svgFileName);
 
